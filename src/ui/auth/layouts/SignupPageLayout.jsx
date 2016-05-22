fee0bacbb75adbf0fdbf046f9e361f8a188be29b
aunnnn <aun.wirawit@gmail.com>
2016-05-22 19:13:21 +0700

    add simple signup

--- src/ui/auth/layouts/SignupPageLayout.jsx
+++ src/ui/auth/layouts/SignupPageLayout.jsx
@@ -7,9 +7,11 @@ export default class SignupPageLayout extends React.Component {
     this.onSubmit = this.onSubmit.bind(this);
     this.state = { errors: {} };
   }
-  onSubmit() {
+  onSubmit(event) {
+    event.preventDefault();
     const email = this.refs.email.value;
     const password = this.refs.password.value;
+    const confirmPassword = this.refs.confirmPassword.value;
     const errors = {};
 
     if (!email) {
@@ -18,23 +20,47 @@ export default class SignupPageLayout extends React.Component {
     if (!password) {
       errors.password = 'Password required';
     }
-    alert('Boo!');
+    if (!confirmPassword) {
+      errors.confirmPassword = 'Please confirm your password';
+    }
+    if (password !== confirmPassword) {
+      errors.passwordMatch = 'Passwords are not matched.';
+    }
+    this.setState({ errors });
+    if (Object.keys(errors).length) {
+      return;
+    }
+    Accounts.createUser({
+      email,
+      password,
+    }, error => {
+      if (error) {
+        console.log('CreateUser error: '+error.reason);
+      } else {
+        console.log('CreateUser success');
+        this.context.router.push('/');
+      }
+    });
   }
   render() {
+    const { errors } = this.state;
+    const errorMessages = Object.keys(errors).map(key => errors[key]);
+    // const errorClass = key => errors[key] && 'error';
     return (
       <div>
         <form onSubmit={this.onSubmit}>
           <div className="row">
             <h4>Signup</h4>
             <div>
-              {}
+              {errorMessages.map(msg => (
+                <p>{msg}</p>
+              ))}
             </div>
             <label>Email</label>
             <input
               className="u-full-width"
               type="email"
               placeholder="Your email"
-              id="exampleEmailInput"
               ref="email"
             />
 
@@ -43,9 +69,18 @@ export default class SignupPageLayout extends React.Component {
               className="u-full-width"
               type="password"
               placeholder="Your password"
-              id="exampleEmailInput"
               ref="password"
             />
+
+            <label>Confirm Password</label>
+            <input
+              className="u-full-width"
+              type="password"
+              placeholder="Confirm password"
+              ref="confirmPassword"
+            />
+
+
             <input className="button-primary" type="submit" value="Login" />
           </div>
         </form>
@@ -53,3 +88,7 @@ export default class SignupPageLayout extends React.Component {
     );
   }
 }
+
+SignupPageLayout.contextTypes = {
+  router: React.PropTypes.object,
+};
