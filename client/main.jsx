441bf450129e9dc9eb8f9a324817846081976d81
Jirat <jirat.onaree@gmail.com>
2016-05-22 09:27:46 +0700

    normalized

--- client/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import App from '../imports/ui/layouts/App.jsx';
-
-Meteor.startup(() => {
-	render(<App />, document.getElementById('react-root'));
-});
--- imports/ui/containers/AppContainer.jsx
+++
@@ -1,7 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { createContainer } from 'meteor/react-meteor-data';
-import App from '../layouts/App.jsx';
-export default createContainer(() => {
-
-
-}, App);
