353a6dbfa31a0a9e9f77e45e8ba58cfdc2f830fa
aunnnn <aun.wirawit@gmail.com>
2016-06-21 00:54:11 +0700

    At last... dummy data!

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,7 +1,102 @@
 import { Meteor } from 'meteor/meteor';
 import { Categories } from '../src/api/thought/categories.js';
+import { Accounts } from 'meteor/accounts-base';
+import { Thoughts } from '../src/api/thought/thoughts.js';
+import { Discussions } from '../src/api/discussion/discussions.js';
 
-Meteor.startup(() => {
+import { _ } from 'meteor/underscore';
+import faker from 'faker';
+
+function dumpDiscussions(embedThought) {
+  if (Discussions.find({ thought_id: embedThought._id }).count() < 10) {
+    _.times(10, () => {
+      if (Math.random() >= 0.4) {
+        const created_by = _.sample(Meteor.users.find().fetch())._id;
+        const first_message = faker.lorem.sentence();
+
+        const discussion = {
+          thought: embedThought,
+          created_by,
+          first_message,
+          latest_message: first_message,
+          last_active: faker.date.recent(10),
+        };
+        Discussions.insert(discussion);
+      }
+    });
+  }
+}
+
+function dumpThoughts(userId) {
+  if (Thoughts.find({ user_id: userId }).count() < 10) {
+    _.times(10, () => {
+      if (Math.random() >= 0.5) {
+        const type = Math.random() >= 0.5 ? 'NORMAL' : 'GLOBAL';
+        const categoryTitle = _.sample(Categories.find().fetch()).title;
+        const thought = {
+          user_id: userId,
+          type,
+          header: faker.lorem.sentence(),
+          description: faker.lorem.paragraph(),
+          category: {
+            title: categoryTitle,
+          },
+        };
+
+        const thoughtId = Thoughts.insert(thought);
+        const embedThought = {
+          _id: thoughtId,
+          user_id: userId,
+          header: thought.header,
+          description: thought.description,
+          category: categoryTitle,
+        };
+        dumpDiscussions(embedThought);
+      }
+    });
+  }
+}
+
+function dumpUsers() {
+  // if number of users less than 30
+  if (Meteor.users.find().count() < 30) {
+    _.times(20, (n) => {
+      
+
+      // fake appProfile
+      const men = Math.random() >= 0.5;
+      const gender = men ? 1 : 0;
+      const sex = men ? 'men' : 'women';
+      const first_name = faker.name.firstName(gender);
+      const last_name = faker.name.lastName();
+      const address = {
+        born: `${faker.address.zipCode()} ${faker.address.streetName()}`,
+        lives: `${faker.address.zipCode()} ${faker.address.streetName()}`,
+      };
+      const description = `${faker.lorem.sentence()} My name is ${first_name}`;
+
+      // register new user
+      const userId = Accounts.createUser({
+        email: faker.internet.email(first_name),
+        password: '1234',
+      });
+
+      const appProfile = {
+        picture: `https://randomuser.me/api/portraits/thumb/${sex}/${n}.jpg`,
+        first_name,
+        last_name,
+        address,
+        description,
+      };
+      Meteor.users.update({ _id: userId }, { $set: { appProfile } });
+
+      // create thoughts for this user
+      dumpThoughts(userId);
+    });
+  }
+}
+
+function dumpCategories() {
   if (Categories.find().count() === 0) {
     const dummyCategoryData = [
       {
@@ -58,4 +153,12 @@ Meteor.startup(() => {
       Categories.insert(category);
     });
   }
+}
+
+
+Meteor.startup(() => {
+  dumpCategories();
+  dumpUsers();
+  // console.log(_.sample(Categories.find().fetch()).title);
+  // console.log(_.sample(Categories.find({}, { fields: { 'title': 1 } })));
 });
--- src/api/feed/feeds.js
+++ src/api/feed/feeds.js
@@ -1,2 +1,99 @@
 import { Mongo } from 'meteor/mongo';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+
+class FeedCollection extends Mongo.Collection {
+
+}
+
+const FriendActivitySchema = new SimpleSchema({
+  friend_info: {
+    type: new SimpleSchema({
+      user_id: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id,
+        label: "Friend's Id",
+      },
+      picture: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Url,
+        label: "Friend's picture url",
+      },
+      first_name: {
+        type: String,
+        min: 0,
+        label: "Friend's first name",
+      },
+      last_name: {
+        type: String,
+        min: 0,
+        label: "Friend's last name",
+      },
+    }),
+  },
+  action: {
+    type: String,
+    allowedValues: [
+      'respond', // respond to Thought
+      'like', // like Thought or Discussion
+      // etc. change profile, thought featured on Top Global Debate
+    ],
+    label: "Action of friend's activity",
+  },
+  object_id: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    label: 'Referenced Thought or Discussion Id (or any Id in the future)',
+  },
+});
+
+const FeedPostSchema = new SimpleSchema({
+  type: {
+    type: String,
+    allowedValues: [
+      'THOUGHT', // personal /global thought
+      'ACTIVITY', // activity of friends, e.g., likes, respond.
+      // etc. SPECIAL: special, custom content from our website
+    ],
+    label: "Type of Feed's Post (THOUGHT or ACTIVITY)",
+  },
+  content: {
+    type: Object,
+    label: 'Content of post, either Thought or Activity',
+  },
+
+});
+
+
+const Feeds = new CategoryCollection('Categories');
+
+Feeds.deny({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; },
+});
+
+Feeds.subSchema_FeedPost = FeedPostSchema;
+Feeds.subSchema_FriendActivity = FriendActivitySchema;
+
+Feeds.schema = new SimpleSchema({
+  user_id: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    label: 'User that owns the feed',
+  },
+
+  year_month: {
+    type: String,
+    regEx: /^[0-9]+$/,
+    label: 'YEARMONTH code (e.g. 201606)',
+  },
+
+  posts: [FeedPostSchema],
+
+
+});
+
+Feeds.attachSchema(Feeds.schema);
+
+export { Feeds };
