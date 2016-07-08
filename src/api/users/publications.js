0ace7c4f73fd3103464377024ab7a605c2647d10
jeojoe <jirat.onaree@gmail.com>
2016-07-08 18:15:27 +0700

    move Account.onCreateUser from publication file to collection file

--- src/api/users/publications.js
+++ src/api/users/publications.js
@@ -1,24 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
-import { Connections } from '../connection/connections.js';
-import { Feeds } from '../feed/feeds.js';
-Accounts.onCreateUser((options, user) => {
-  // add app-related user data to a new field
-
-  user.appProfile = {};
-
-  if (user.services.facebook) {
-    const fbId = user.services.facebook.id;
-    user.appProfile.picture = `http://graph.facebook.com/${fbId}/picture/?type=large`;
-    user.appProfile.first_name = user.services.facebook.first_name;
-    user.appProfile.last_name = user.services.facebook.last_name;
-  }
-
-  // make first time connection bucket
-  Connections.insert({ user_id: user._id });
-  Feeds.insert({ user_id: user._id });
-  return user;
-});
 
 Meteor.publish('userData', function userData() {
   const user = Meteor.users.find({ _id: this.userId }, {
--- src/api/users/users.js
+++ src/api/users/users.js
@@ -1,3 +1,7 @@
+import { Accounts } from 'meteor/accounts-base';
+import { check } from 'meteor/check';
+import { Connections } from '../connection/connections.js';
+import { Feeds } from '../feed/feeds.js';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 export const UserAppProfileSchema = new SimpleSchema({
@@ -32,4 +36,32 @@ export const UserAppProfileSchema = new SimpleSchema({
     type: String,
   },
 
+});
+
+Accounts.onCreateUser((options, user) => {
+  // add app-related user data to a new field
+
+  const isFacebook = check(user.services.facebook, Object);
+
+  const picture = isFacebook ?
+      `http://graph.facebook.com/${user.services.facebook.id}/picture/?type=large`
+      :
+      'http://i.memeful.com/media/post/3M2b6M2_700w_0.jpg';
+
+  const first_name = isFacebook ?
+      user.services.facebook.first_name : '';
+
+  const last_name = isFacebook ?
+      user.services.facebook.last_name : '';
+
+  user.appProfile = {
+    picture,
+    first_name,
+    last_name,
+  };
+
+  // make first time connection bucket
+  Connections.insert({ user_id: user._id });
+  Feeds.insert({ user_id: user._id });
+  return user;
 });
