db4dbb680ae0aadfad59003df4b5aae551777ef1
jeojoe <jirat.onaree@gmail.com>
2016-07-09 13:41:01 +0700

    fix import in AddCategoryContainer

--- src/ui/feed/containers/AddCategoryContainer.jsx
+++ src/ui/feed/containers/AddCategoryContainer.jsx
@@ -1,11 +1,12 @@
 import { createContainer } from 'meteor/react-meteor-data';
 
 import AddCategoryPage from '../layouts/AddCategoryPage.jsx';
-import { Categories } from '../../../api/thought/categories.js';
+import { Categories } from '../../../api/category/categories.js';
 
 export default createContainer(({ currentUser }) => {
-  const allCategories = Categories.methods.getAllCategories.call();
-  console.log(allCategories);
+  const allCategories = Categories.methods.getAllCategories.call((err, result) => {
+    console.log(result);
+  });
   const followedCategories = currentUser.followed_categories ? currentUser.followed_categories : [];
   return {
     allCategories,
