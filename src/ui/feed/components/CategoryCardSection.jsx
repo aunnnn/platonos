6e7174a53b08cf241fbafd3cc1197ac9baba34bc
jeojoe <jirat.onaree@gmail.com>
2016-07-09 15:15:22 +0700

    add CategoryCardSection component

---
+++ src/ui/feed/components/CategoryCardSection.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+import CategoryCard from './CategoryCard.jsx';
+import { Categories } from '../../../api/category/categories.js';
+
+export class CategoryCardSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isQueried: false,
+      categories: [],
+    };
+    this.queryAllCategories = this.queryAllCategories.bind(this);
+  }
+  queryAllCategories() {
+    Categories.methods.getAllCategories.call(
+      (err, result) => {
+        if (err) {
+          console.log('error');
+          return;
+        }
+        this.setState({
+          isQueried: true,
+          categories: result,
+        });
+      }
+    );
+  }
+  render() {
+    const { type } = this.props;
+
+    if (type === 'all') {
+      if (!this.state.isQueried) this.queryAllCategories();
+      return (
+        <div>
+          {this.state.categories.map((category) => (
+            <CategoryCard
+              key={category.title}
+              category={category}
+            />
+          ))}
+        </div>
+      );
+    }
+
+    if (type === 'followed') {
+      return (
+        <div>
+          {this.props.followedCategories.map((category) => (
+            <CategoryCard
+              key={category.title}
+              category={category}
+            />
+          ))}
+        </div>
+      );
+    }
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+CategoryCardSection.propTypes = {
+  type: React.PropTypes.string.isRequired,
+  followedCategories: React.PropTypes.array,
+};
+
+export default CategoryCardSection;
