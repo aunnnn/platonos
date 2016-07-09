a21d8f22bb165ad94b6506eeb55157d2b677051c
jeojoe <jirat.onaree@gmail.com>
2016-07-10 00:33:55 +0700

    Bug Fix for Following Category

--- src/ui/feed/layouts/CategoryFeed.jsx
+++ src/ui/feed/layouts/CategoryFeed.jsx
@@ -74,16 +74,41 @@ class CategoryFeed extends React.Component {
   }
 
   render() {
-    const {
-      categoryName,
-      currentUser,
-    } = this.props;
+    const { categoryTitle, currentUser, dispatch } = this.props;
+    const { categoryObj } = this.state;
+
+    let isFollowed = false;
+    currentUser.appProfile.followed_categories.forEach(
+      (category) => {
+        if (category.title === categoryTitle) isFollowed = true;
+      }
+    );
+
     return (
       <div>
-        <div className="category-feed-header">
-          <h4 className="feed-header">{categoryName}</h4>
-          <div className="unfollow-button">Unfollow</div>
-          <p className="merr-font">Interesting thoughts in {` ${categoryName}`}</p>
+        <div className="cfh">
+          <h4 className="feed-header">{categoryTitle}</h4>
+          {
+            isFollowed ?
+              <div
+                className="button"
+                onClick={() => {
+
+                }}
+              >
+                Unfollow
+              </div>
+              :
+              <div
+                className="button follow"
+                onClick={() => {
+                  dispatch(followCategory(categoryObj));
+                }}
+              >
+                Follow
+              </div>
+          }
+          <p className="merr-font">Interesting thoughts in {` ${categoryTitle}`}</p>
         </div>
         {this.getDummyData().map(
           thought =>
