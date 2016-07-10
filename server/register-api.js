aad1ca40d2346695356feae5f4aa2cb03a670308
jeojoe <jirat.onaree@gmail.com>
2016-07-10 16:09:39 +0700

    reorganize register-api on server, import users.js

--- server/register-api.js
+++ server/register-api.js
@@ -1,18 +1,24 @@
-// users api
-import '../src/api/users/publications.js';
-import '../src/api/users/methods.js';
+// category api
+import '../src/api/category/methods.js';
 
-// thought api
-import '../src/api/thought/publications.js';
-import '../src/api/thought/methods.js';
+// connection api
 
 // discussion api
-// import '../src/api/discussion/publications.js';
 import '../src/api/discussion/methods.js';
 
 // feed api
 import '../src/api/feed/methods.js';
 import '../src/api/feed/publications.js';
 
-// category api
-import '../src/api/category/methods.js';
+// subdiscussion api
+
+// thought api
+// import '../src/api/thought/thoughts.js';
+// import '../src/api/thought/draftThoughts.js';
+import '../src/api/thought/publications.js';
+import '../src/api/thought/methods.js';
+
+// users api
+import '../src/api/users/users.js';
+import '../src/api/users/publications.js';
+import '../src/api/users/methods.js';
