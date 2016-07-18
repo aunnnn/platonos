30c19350a4cf3a531109af092830c0c7768307b7
jeojoe <jirat.onaree@gmail.com>
2016-07-18 23:19:59 +0700

    register all files in api domain (for 100% functional app startup)

--- client/register-api.js
+++ client/register-api.js
@@ -1,14 +1,28 @@
-// users api
-import '../src/api/users/methods.js';
+// category api
+import '../src/api/category/categories.js';
+import '../src/api/category/methods.js';
 
-// thought api
-import '../src/api/thought/methods.js';
+// connection api
+import '../src/api/connection/connections.js';
+import '../src/api/connection/methods.js';
 
 // discussion api
+import '../src/api/discussion/discussions.js';
 import '../src/api/discussion/methods.js';
 
 // feed api
+import '../src/api/feed/feeds.js';
+import '../src/api/feed/actions.js';
+import '../src/api/feed/methods.js';
 
+// subdiscussion api
+import '../src/api/subdiscussion/subdiscussions.js';
 
-// category api
-import '../src/api/category/methods.js';
+// thought api
+import '../src/api/thought/thoughts.js';
+import '../src/api/thought/draftThoughts.js';
+import '../src/api/thought/methods.js';
+
+// users api
+import '../src/api/users/users.js';
+import '../src/api/users/methods.js';
