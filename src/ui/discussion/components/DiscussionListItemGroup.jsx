2e4cfc22e7d9c99ed5a02bdcf050b01af8a706ed
jeojoe <jirat.onaree@gmail.com>
2016-07-23 12:29:10 +0700

    transform DiscussionListItemGroup to pure component

--- src/ui/discussion/components/DiscussionListItemGroup.jsx
+++ src/ui/discussion/components/DiscussionListItemGroup.jsx
@@ -1,69 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router';
 import classNames from 'classnames';
 
 import DiscussionListItem from './DiscussionListItem.jsx';
 import './DiscussionListItemGroup.import.css';
 
-class DiscussionListItemGroup extends Component {
+const DiscussionListItemGroup = ({ discussionGroup, isActive, changeActiveGroup, changeActiveDiscussion, currentUser, activeDiscussion, router, groupThatHasActiveDiscussion }) => {
+  const isOwner = currentUser._id === discussionGroup.thought.user_id;
+  const hasActiveDiscussion = groupThatHasActiveDiscussion === discussionGroup.thought._id;
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const {
-      discussionGroup, isActive, changeActiveGroup,
-      changeActiveDiscussion, currentUser,
-      activeDiscussion, router, groupThatHasActiveDiscussion,
-    } = this.props;
-
-    const isOwner = currentUser._id === discussionGroup.thought.user_id;
-    const hasActiveDiscussion = groupThatHasActiveDiscussion === discussionGroup.thought._id;
-    return (
-      <div className="dl-ig">
-        <div
-          className={classNames('group-header', { active: isActive }, { hasActiveDiscussion })}
-          onClick={() => { changeActiveGroup(discussionGroup.thought._id); }}
-        >
-          <i className="fa fa-lightbulb-o"></i>
-          <label>{discussionGroup.thought.category}</label>
-          {
-            isOwner ? <label className="own">Your thought</label> : ''
-          }
-          <h5>{discussionGroup.thought.header}</h5>
-          {isOwner ?
-            <div className="noti">
-              <div className="item new-message">3 New messages</div>
-              {/*<div className="item new-discussion">1 New discussions</div>*/}
-            </div> : ''
-          }
-        </div>
-
-        {// render discussions
-          isActive ?
-            discussionGroup.discussions.map(discussion => {
-              if (!hasActiveDiscussion) {
-                if (activeDiscussion !== discussion._id) {
-                  if (router.isActive(`/discussions/${discussion._id}`)) {
-                    changeActiveDiscussion(discussion._id, discussionGroup.thought._id);
-                  }
-                }
-              }
-              return (
-                <DiscussionListItem
-                  discussion={discussion}
-                  key={discussion._id}
-                />
-              );
-            })
-            :
-            ''
+  return (
+    <div className="dl-ig">
+      <div
+        className={classNames('group-header', { active: isActive }, { hasActiveDiscussion })}
+        onClick={() => { changeActiveGroup(discussionGroup.thought._id); }}
+      >
+        <i className="fa fa-lightbulb-o"></i>
+        <label>{discussionGroup.thought.category}</label>
+        {
+          isOwner ? <label className="own">Your thought</label> : ''
+        }
+        <h5>{discussionGroup.thought.header}</h5>
+        {isOwner ?
+          <div className="noti">
+            <div className="item new-message">3 New messages</div>
+            {/*<div className="item new-discussion">1 New discussions</div>*/}
+          </div> : ''
         }
       </div>
-    );
-  }
-}
+
+      {// render discussions
+        isActive ?
+          discussionGroup.discussions.map(discussion => {
+            if (!hasActiveDiscussion) {
+              if (activeDiscussion !== discussion._id) {
+                if (router.isActive(`/discussions/${discussion._id}`)) {
+                  changeActiveDiscussion(discussion._id, discussionGroup.thought._id);
+                }
+              }
+            }
+            return (
+              <DiscussionListItem
+                discussion={discussion}
+                key={discussion._id}
+              />
+            );
+          })
+          :
+          ''
+      }
+    </div>
+  );
+};
 
 DiscussionListItemGroup.propTypes = {
   discussionGroup: React.PropTypes.object,
